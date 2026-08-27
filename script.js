3be8007104720ed815f9e2c9146ecc9c6774b182
harshbagoria <139875633+harshbagoria@users.noreply.github.com>
2026-08-28 00:47:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 //your JS code here. If required.
 const array = [1, 2, 3, 4];
-
-const filterArray = (arr) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const filteredArray = arr.filter((item) => item % 2 === 0);
-      resolve(filteredArray);
-    }, 1000);
-  }).then((filteredArray) => {
-    console.log(filteredArray);
+const output = document.getElementById("output");
+const initialPromise = new Promise((resolve , reject) => {
+  setTimeout(() => {
+    resolve(array);
+  }, 3000);
+});
+initialPromise.then((arr) =>{
+	const filteredArray = arr.filter((item) => item % 2 === 0);
+	 return new Promise((resolve , reject) => {
+      setTimeout(() => {
+        output.textContent = filteredArray.join(",");
+        resolve(filteredArray);
+      }, 1000);
+    });
+  })
+.then((filteredArray) =>{
+	 const multipliedArray = filteredArray.map((item) => item * 2);
 
     return new Promise((resolve) => {
       setTimeout(() => {
-        const multipliedArray = filteredArray.map((item) => item * 2);
+        output.textContent = multipliedArray.join(",");
         resolve(multipliedArray);
-      }, 3000);
+      }, 2000);
     });
-  });
-};
-
-filterArray(array)
-  .then((result) => {
-    const data = document.createElement("h1");
-    data.textContent = "Multiplied array: " + result.join(", ");
-    document.body.appendChild(data);
   })
-  .catch((error) => {
+.catch((error) => {
     console.error("Error:", error);
   });
+	
